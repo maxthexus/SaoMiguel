f42980ae50942fcc415ab4d81f954dc2b7ed8904
Matheus Duarte <matheus_14net@hotmail.com>
2024-01-09 16:28:22 -0300

    ok

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function animar(){
+    const btn = document.getElementById('btn-menu')
+    const menu = document.getElementById('menu-mobile')
+    btn.classList.toggle('ativar')
+    menu.classList.toggle('ativar')
+
+    
+}
